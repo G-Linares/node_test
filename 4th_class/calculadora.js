75dc9b916091fe2da819ccd9f189a84e67b95e6f
G-Linares <gerrylinares@gmail.com>
2021-09-30 21:27:54 -0500

    modulo mismo nombre variable

--- 4th_class/calculadora.js
+++ 4th_class/calculadora.js
@@ -4,22 +4,22 @@ let mimodulo = require('./opes/todo');
 
 /// vienen un modulo por cada operacion
 
-let sumaind = require('./opes/sumar');
-let restaind = require('./opes/restar');
-let multiplicacionind = require('./opes/multiplicar');
-let divisionind = require('./opes/dividir');
+let suma = require('./opes/sumar');
+let resta = require('./opes/restar');
+let multiplicacion = require('./opes/multiplicar');
+let division = require('./opes/dividir');
 
 //imprimo resultado de modulo multiple
 
-console.log("La suma es: " + (mimodulo.sumardetodo(1,2)));
-console.log("La resta es: " + (mimodulo.restardetodo(10,2)));
-console.log("La multiplicacion es: " + (mimodulo.multiplicardetodo(5,10)));
-console.log("La division es: " + (mimodulo.dividirdetodo(100,50)));
+console.log("La suma es: " + mimodulo.sumardetodo(1,2));
+console.log("La resta es: " + mimodulo.restardetodo(10,2));
+console.log("La multiplicacion es: " + mimodulo.multiplicardetodo(5,10));
+console.log("La division es: " + mimodulo.dividirdetodo(100,50));
 
 
 /// imprimo resultado de modulos separados
 
-console.log("\nLa suma es: " + sumaind.sumarind(1, 2));
-console.log("La resta es: " + restaind.restarind(10, 2));
-console.log("La multiplicacion es: " + multiplicacionind.multiplicarind(2,10));
-console.log("La division es: " + divisionind.dividirind(100,0));
+console.log("\nLa suma es: " + suma.sumarind(1, 2));
+console.log("La resta es: " + resta.restarind(10, 2));
+console.log("La multiplicacion es: " + multiplicacion.multiplicarind(2,10));
+console.log("La division es: " + division.dividirind(100,0));
